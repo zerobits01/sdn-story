dce5ec8f9a89f680e7b0ceb234ecc16614cf3733
zerobits01 <zerobits0101@gmail.com>
2020-08-09 12:32:33 +0430

    last test for telnet doesn't work :(

---
+++ test-apps/test-telnet.py
@@ -0,0 +1,49 @@
+"""
+author : zerobits01
+purpose: testing if how to interact with floodlight
+created: 9-Aug-2020  11:15
+modified: 9-Aug-2020 11:15
+"""
+
+import getpass
+import subprocess
+import os
+
+# HOST = "172.16.229.129"
+# user = input("Enter your remote account: ")
+# password = getpass.getpass()
+
+print("[+] started")
+
+subprocess.run(["sleep", "2", "echo", "-e", "sdn\n"], stdout=subprocess.PIPE)
+t = subprocess.run(['telnet', "-l", "zerobits", "172.16.229.129", "32769"], shell=True, capture_output=True, check=True, stdin=subprocess.PIPE)
+# t = subprocess.run(["echo" , "sdn"], stdout=subprocess.PIPE)
+
+print(t.stdout)
+
+
+
+
+
+"""
+import getpass
+import telnetlib
+
+HOST = "localhost"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"login: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
+"""
+
