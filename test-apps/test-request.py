54f63e79b62b7e060b6f7a1246163e7e127b6e12
zerobits01 <zerobits0101@gmail.com>
2020-08-09 12:30:34 +0430

    request test works Ok! :)

---
+++ test-apps/test-request.py
@@ -0,0 +1,35 @@
+"""
+author : zerobits01
+purpose: testing if how to interact with floodlight
+created: 9-Aug-2020 11:07
+modified: 9-Aug-2020 11:07
+"""
+
+import requests
+import sys
+
+enable_url = f"http://{sys.argv[1]}:8080/wm/firewall/module/enable/json"
+add_url = f"http://{sys.argv[1]}:8080/wm/firewall/rules/json"
+
+def testEnableFirewall():
+    # TODO : request to enable firewall on floodlight
+    r = requests.put(enable_url)
+    print(r)
+
+def testAddRule():
+    # TODO : testing adding a simple rule
+    data = {"src-ip": "10.0.0.4/32", "dst-ip": "10.0.0.5/32", "nw-proto":"ICMP"}
+    r = requests.post(url=add_url, data=data)
+    print(r.content)
+
+def testPrintRules():
+    r = requests.get(url=add_url)
+    print(r.content)
+
+if __name__ == "__main__":
+    testEnableFirewall()
+    testAddRule()
+    testPrintRules()
+
+
+# Test Done correctly
